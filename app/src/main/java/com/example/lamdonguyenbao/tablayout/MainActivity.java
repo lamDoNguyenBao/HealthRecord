0a5dd5c8c54611b345f9ebafb2d69bda5a25d5e2
Your Name <you@example.com>
2019-03-12 21:30:14 +0700

    first commit

---
+++ app/src/main/java/com/example/lamdonguyenbao/tablayout/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.lamdonguyenbao.tablayout;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lamdonguyenbao.tablayout.fragment.HistoryFragment;
+import com.example.lamdonguyenbao.tablayout.fragment.HospitalFragment;
+import com.example.lamdonguyenbao.tablayout.fragment.InforFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        setupViewPager();
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+    }
+
+    private int[] tabIcons = {
+            R.drawable.ic_filter_1_white_24dp,
+            R.drawable.ic_filter_2_white_24dp,
+            R.drawable.ic_filter_3_white_24dp
+    };
+
+    private void setupViewPager() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new InforFragment(), "Information");
+        adapter.addFragment(new HistoryFragment(), "History");
+        adapter.addFragment(new HospitalFragment(), "Hospital");
+        viewPager.setAdapter(adapter);
+    }
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+    }
+}
---
+++ app/src/main/java/com/example/lamdonguyenbao/tablayout/fragment/HospitalFragment.java
@@ -0,0 +1,25 @@
+package com.example.lamdonguyenbao.tablayout.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lamdonguyenbao.tablayout.R;
+
+public class HospitalFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_hospital, container, false);
+    }
+
+}
